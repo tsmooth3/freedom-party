bbee69d599c8f739895ad41bea1c6bde2399ca9a
tsmooth3 <tstewart3@gmail.com>
2024-06-05 01:36:41 -0400

    event watcher

--- src/routes/api/eventRounds/[eventId]/+server.ts
+++ src/routes/api/eventRounds/[eventId]/+server.ts
@@ -30,38 +30,3 @@ export const GET: RequestHandler = async ({ params }) => {
         return json({ success: false, message: error })
     }
 };
-
-// export const PUT: RequestHandler = async ({ url, request }) => {
-//     let eventId: number | undefined = Number(url.searchParams.get('eventId'))
-//     let teamId: number | undefined = Number(url.searchParams.get('teamId'))
-//     let roundIndex: number | undefined = Number(url.searchParams.get('roundIndex'))
-//     let body: prismaEventRound = await request.json()
-//     let state: EventState
-//     if (body.roundClays.match("-")) {
-//         state = "ACTIVE"
-//     } else {
-//         state = "COMPLETE"
-//     }
-//     if (eventId === body.eventId && teamId === body.teamId && roundIndex === body.roundIndex) {
-//         try {
-//             const eventRound = await prisma.eventRound.update({
-//                 where: {
-//                     id: body.id,
-//                     eventId: eventId,
-//                     teamId: teamId,
-//                     roundIndex: roundIndex,
-//                 },
-//                 data: {
-//                     roundAmmo: body.roundAmmo,
-//                     roundClays: body.roundClays,
-//                     roundState: state
-//                 }
-//             })
-//             return json({ success: true })
-
-//         } catch (error) {
-//             return json({ success: false, message: error })
-//         }
-//     }
-//     return json({ success: false })
-// };
---
+++ src/routes/watchEvent/[eventId]/+page.server.ts
@@ -0,0 +1,6 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+    const id = params.eventId
+    return {id};
+}
