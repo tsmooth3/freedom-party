ac8334c42b34c976332825c327bddab925db41f4
tsmooth3 <tstewart3@gmail.com>
2024-07-05 19:55:16 -0400

    slide

--- src/routes/slide/+page.server.ts
+++ src/routes/slide/+page.server.ts
@@ -4,14 +4,9 @@ import type { prismaSlide } from "$lib/shared/utils";
 import { invalidateAll } from "$app/navigation";
 
 export const load: PageServerLoad = async ({ fetch }) => {
-    try {
-        const dbSlidesResponse = await fetch('/api/slides')
-        const dbSlides: prismaSlide[] = await dbSlidesResponse.json()
-        return { dbSlides}
-    } catch (error) {
-        const dbSlides: prismaSlide[] = []
-        return { dbSlides }
-    }
+    const dbSlidesResponse = await fetch('/api/slides')
+    const dbSlides: prismaSlide[] = await dbSlidesResponse.json()
+    return { dbSlides }
 };
 
 export const actions: Actions = {
