11aa2dcd555f77bbeb7166a020ce00ce41722f96
tsmooth3 <tstewart3@gmail.com>
2024-07-23 15:20:36 -0400

    speech

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import wethepeople from '$lib/images/wethepeople.jpg'
 </script>
-<div class="flex min-w-[390px] max-w-6xl my-auto mx-auto ">
+<div class="flex-col min-w-[390px] max-w-6xl my-auto mx-auto ">
 	<div class="card card-hover m-3 overflow-hidden variant-outline-primary">
 		<header>
 			<img src={wethepeople} alt="Post" />
@@ -104,5 +104,140 @@
 				<small>Freeland {new Date(2023, 6, 1).toDateString()}</small>
 			</div>
 		</footer>
+	</div>
+	<div class="card card-hover m-3 overflow-hidden variant-outline-primary">
+		<div class="p-4 space-y-4">
+            <h2 class="h2">
+                Operation Celebrate Freedom
+                2024
+            </h2>
+			<article>
+                <p class="my-2 p-2">
+                    
+                    First thanks to all for being here to celebrate freedom. 
+                </p><p class="my-2 p-2">
+                    
+                    Save the date for next year… Saturday July 5, 2025. If ever in doubt, closest Saturday to July 4 never in June. Invite all your friends. 
+                </p><p class="my-2 p-2">
+                    
+                    Thanks most to Emily. God bless Emily. 
+                </p><p class="my-2 p-2">
+                    
+                    Every year that goes by I relate to John Adams' letter to Abigail Adams more deeply. Many of you are familiar with these words he wrote on July 3rd, 1776, “I am apt to believe that this day will be celebrated by succeeding generations as the great anniversary festival. It ought to be commemorated as the day of deliverance by solemn Acts of Devotion to God Almighty. It ought to be solemnized with Pomp and Parade, with Shows (and speeches), Games, Sports, Guns, Bells, Bonfires and Illuminations from one End of this Continent to the other from this Time forward forever more.” 
+                </p><p class="my-2 p-2">
+                    
+                    We are doing pretty good so far… and we are only part way through it. But now it is time to add the speech…. And don’t worry kiddos, unless I get rambling (or crying) it should take no more than 7 minutes…
+                </p><p class="my-2 p-2">
+                    
+                    It is interesting that after the quote I just read, John Adams went on to forecast that he was “well aware of the Toil and Blood and Treasure that it will cost us to maintain this Declaration and support and defend the states”
+                </p><p class="my-2 p-2">
+                    
+                    Today, while not about to fight a literal revolution, there are still “costs to maintain the declaration”. Different types of costs perhaps but costs nonetheless. As we look around us, culture, politicians, leaders, media - seem to be trying to not only divide us but shame us. Some want us to be ashamed of America’s past, others want us to be ashamed of America’s present, the result for many is that we are tempted to be afraid for America’s future. 
+                </p><p class="my-2 p-2">
+                    
+                    In my reflections, I wanted to share some thoughts on this because these messages are rubbing off on me and I don’t even own a TV. 
+                </p><p class="my-2 p-2">
+                    
+                    I was talking with a respected colleague about a work meeting we had last month, hosted at a historic property in Georgia. I said how I enjoyed the setting because it felt like “going back in time”. Without missing a beat, the lady I was talking with said in an offensive tone, “you mean, back in a time when women couldn’t vote”? 
+                </p><p class="my-2 p-2">
+                    
+                    America has never been and never will be perfect. But I will not be ashamed of our country or the amazing men and women over the generations who have contributed, some by paying the ultimate sacrifice, to establish, to protect and to preserve this land. I will not be ashamed of the vision that founded our country, one nation under God, a belief that we are endowed by our Creator with unalienable rights - life, liberty and the pursuit of happiness. 
+                </p><p class="my-2 p-2">
+                    
+                    But how do we respond when we feel afraid or pressured by culture to be ashamed of our country? I am actually very curious of your thoughts… But right now, I’m going to share mine. 
+                </p><p class="my-2 p-2">
+                    
+                    As I wrestled with this, I realize many of us are used to this already. As believers in God and His Son, Jesus Christ, we know we are called NOT to be ashamed of our faith, but to proclaim the incredible life changing news that by believing in Christ’s existence as God’s Son, His sacrifice and His resurrection, we are not only saved from our sin but blessed with peace and hope beyond measure (more on that another time)….
+                </p><p class="my-2 p-2">
+                    
+                    As Christian’s, we are called to be ready in season and out of season to share this great news! To convince, rebuke and exhort with all longsuffering and teaching. That verse goes on to say that a time will come when they will not endure sound doctrine but heap up for themselves teachers to scratch their itching ears - that they will turn their ears away from the truth and be turned aside to fables. Sound familiar?
+                </p><p class="my-2 p-2">
+                    
+                    What does any of this have to do with my colleague getting upset at me for enjoying history and what does any of that have to do with the Fourth of July?
+                </p><p class="my-2 p-2">
+                    
+                    I believe how we respond to a culture who shames us for our beliefs and history is part of our responsibility as Americans. So how do we do this?
+                </p><p class="my-2 p-2">
+                    
+                    I have no easy answer, but I do have a simple one. And I hope you are encouraged by it.
+                </p><p class="my-2 p-2">
+                    
+                    While I believe God will make it clear how to handle each situation we encounter if we stay in tune with Him, He has also given us, in His Word, a vision and a calling for freedom in Christ and how to live out our freedom.
+                </p><p class="my-2 p-2">
+                    
+                    “For you brethren, have been called to liberty; only do not use liberty as an opportunity for the flesh, but through love serve one another. For all the law is fulfilled in one word, even in this: you shall love your neighbor as yourself… (Gal 5.13-14)
+                </p><p class="my-2 p-2">
+                    
+                    Earlier on in that chapter, God says this even more succinctly - “Faith that finds its expression in love is all that matters.” (Gal 5.6)
+                </p><p class="my-2 p-2">
+                    
+                    So when leaders (or respected colleagues) tell us to be ashamed of our past</p>
+                <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span> 
+                </p><p class="my-2 p-2">
+                    
+                    When others tell us to be ashamed of our present,</p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    When you are tempted to be afraid for the future,</p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+        
+                    <i>kids… help me here</i> 
+                </p><p class="my-2 p-2">
+                        
+                    When media, music or insta chat tell us our country is evil,</p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    When we are tempted to stay quiet or worse yet, feel silenced, let your actions speak louder than your words and </p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    When you are tempted to feel helpless, remember to prioritize the incredible opportunity you have to influence the people you interact with everyday and </p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    When it feels like there is a failure of leadership  and no good options to choose from, remember the only real leadership is servant leadership so </p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    And as we hold on to the hope that the true Leader of Leaders is not dead but alive and will come soon to have His way, let us </p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    Kids, look around. Friends, be encouraged and keep it up. Keep serving one another in love. We stand in great company and I will not be ashamed. I will not be afraid. We are committed to our marriages, to parenting and grand-parenting our families, to raising our children, to schooling the next generation, to building homes and houses, restoring rental properties, engineering road ways, hanging  power lines, reinsuring nuclear power plant projects, data analytics, AI, IT (I actually have no idea what Tim and Brian do, but I know they are geniuses), financial advising, loan officing, teaching, coaching, caring for sick patients (and making sure they have good health insurance), starting new enterprises, leading teams of people, giving back to the church and broader community… Friends, keep it up, </p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    And for those of you who are still not convinced and remain concerned about the future, look around at these kids… and dont blink… they are already making their mark… running Etsy shops, publishing and editing magazines, cooking meals, devouring classic literature, managing bank accounts, training dogs, performing theater, playing music, writing code, drawing masterpiece artwork, winning baseball games, building chicken coops, disassembling pianos, hiking the Appalachian Trail, farming goats, cutting lawns, hunting and trapping, writing books, helping teach homeschool… Parents, your leadership is present, your faith, your heart, your attitude, your service is already producing great fruit. Kids, keep it up </p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-blue-500">Serve one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    And if you are still not satisfied, remember our hope. John Adams finished the letter to Abigail saying that “through all the Gloom I can see the Rays of ravishing Light and Glory. I can see that the End is more than worth all the Means. And that Posterity will triumph, even if we should regret it, which I trust in God we shall not.” Talk about Rays of Ravishing Light and Glory… God tells us and our founding fathers lived it… that hard times produce endurance and endurance produces character and character produces hope. 
+                </p><p class="my-2 p-2">
+                    
+                    The hope we have as those who believe in Christ Jesus is this - He is coming back. He will return. He will make all things right. We will look Him in the eye and finally be like Him. We will be redeemed and restored and finally and fully free to experience God in His fullness not only on a restored and perfected earth BUT ALSO in the heavenly places where we will be with Christ in His glory above all rule and authority and power and dominion, when the last enemy, death, is destroyed and all things are made subject to the Son and the Son Himself will be subject to Him who put all things under Him, that God may be all in all. Friends, don’t lose heart, keep </p>
+                    <p class="my-1 px-10"> <span class="highlight highlight-variant-3 highlight-red-500">Serving one another in love.</span>
+                </p><p class="my-2 p-2">
+                    
+                    God bless America!!
+                </p><p class="my-2 p-2">
+                    
+                    Kate S. - National Anthem.
+                </p><p class="my-2 p-2">
+                    
+                    Prayer
+                </p><p class="my-2 p-2">
+				</p>
+			</article>
+    		<hr class="opacity-50" />
+	    	<footer class="p-4 flex justify-start items-center space-x-4">
+                <div class="flex-auto flex justify-between items-center">
+                    <h6 class="font-bold">Justin and Emily</h6>
+				    <small>Freeland {new Date(2024, 6, 6).toDateString()}</small>
+			    </div>
+		    </footer>
+	    </div>
 	</div>
 </div>
